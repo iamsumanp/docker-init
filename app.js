c7264d79698669768dc8a6b051b22e1aac294943
Suman <sumanpok9@gmail.com>
2023-12-26 23:47:27 +0545

    set up demo express app

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json([
+    {
+      id: 1,
+      title: 'Severance',
+    },
+    {
+      id: 2,
+      title: 'The BOYS',
+    },
+  ]);
+});
+
+app.listen(4000, () => {
+  console.log('Express project is running on port 4000');
+});
